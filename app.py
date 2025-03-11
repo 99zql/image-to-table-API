ae217488437ab50e21cc879b30d58bc679b9e7b0
99zql <166954461+99zql@users.noreply.github.com>
2025-03-11 19:35:12 -0300

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from PIL import Image, ImageSequence
 app = FastAPI()
 
 @app.get("/imageapi")
-def image_to_table(imageurl: str, resolution: float = Query(1.0, gt=0, le=1.0)):
+def image_to_table(imageurl: str, resolution: float = Query(1.0, gt=0, le=10.0)):
     try:
         response = requests.get(imageurl, timeout=10)
         response.raise_for_status()
@@ -26,11 +26,11 @@ def image_to_table(imageurl: str, resolution: float = Query(1.0, gt=0, le=1.0)):
             for y in range(new_height):
                 for x in range(new_width):
                     r, g, b, a = frame.getpixel((x, y))
-                    roblox_alpha = round(1 - (a / 255), 2)
-                    roblox_alpha = int(roblox_alpha) if roblox_alpha == 0 else roblox_alpha
+                    alpha = round(1 - (a / 255), 2)
+                    alpha = int(alpha) if alpha == 0 else alpha
 
-                    if roblox_alpha < 1:
-                        pixels.append(f"{x},{y},{r},{g},{b},{roblox_alpha}")
+                    if alpha < 1:
+                        pixels.append(f"{x},{y},{r},{g},{b},{alpha}")
 
             if pixels:
                 frames[f"Frame {i+1}"] = pixels
@@ -42,6 +42,5 @@ def image_to_table(imageurl: str, resolution: float = Query(1.0, gt=0, le=1.0)):
 
     except requests.exceptions.RequestException as e:
         raise HTTPException(status_code=400, detail=f"Erro ao baixar a imagem: {e}")
-
     except Exception as e:
         raise HTTPException(status_code=500, detail=f"Erro ao processar a imagem: {e}")
